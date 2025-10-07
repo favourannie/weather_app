84a096743314da63742441c546ac94c07bc770d5
favourannie <javour0422@gmail.com>
2025-10-07 14:33:16 +0100

    Error handling

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -3,14 +3,14 @@ const axios = require("axios")
 exports.weather = async(req,res)=>{
     try {
         const {city} = req.query
-        if(city == null){
+        if(city === null){
             return res.status(400).json({
                 message: "City name is required"
             })
         }
         const apiKey = process.env.WEATHER_SECRET
 
-        if(apiKey == null){
+        if(apiKey === null){
             return res.status(400).json({
                 message: "Api key is required"
             })
@@ -37,6 +37,14 @@ exports.weather = async(req,res)=>{
         if(error.response && error.response.status === 404){
             return res.status(404).json({
                 message: "City not found"
+            })
+        } else if (error.response && error.response.status === 401){
+            return res.status(401).json({
+                message: "Invalid API key"
+            })
+        } else if (error.response && error.response.status === 400){
+            return res.status(400).json({
+                message: "Bad Request"
             })
         }
         res.status(500).json({
